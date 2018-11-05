21c1fc967d42b8966f21e5dcfa61c141097ff210
kelvin3094 <kelvin309413243@hotmail.com>
2018-11-05 14:57:58 -0500

    Adding reading, writing, learning, and speaking skill builders/interfaces

---
+++ teq-data-platform/src/main/java/com/devlopp/teq/skills/SpeakingSkill.java
@@ -0,0 +1,18 @@
+package com.devlopp.teq.skills;
+
+import com.devlopp.teq.course.Course;
+
+public class SpeakingSkill extends Course {
+
+	protected int level;
+	protected String value;
+
+    public int getLevel() {
+        return level;
+    }
+    
+    public String getValue() {
+        return value;
+    }
+}
+    
