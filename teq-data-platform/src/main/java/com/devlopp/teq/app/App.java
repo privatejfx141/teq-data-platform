89c83a5d3c150b0f4a04a25f1b4da6a42f3dea41
Jeffrey L <jeffclub51@yahoo.ca>
2018-11-07 18:32:45 -0500

    Create course enroll and exit entities

--- teq-data-platform/src/main/java/com/devlopp/teq/app/App.java
+++ teq-data-platform/src/main/java/com/devlopp/teq/app/App.java
@@ -25,52 +25,6 @@ public class App {
             DatabaseDriverHelper.initializeDatabase();
         }
 
-        List<Client> clients = ExcelDriver.readClientProfile(filePath, 0);
-        for (Client client : clients) {
-            // insert client next
-            int clientId = DatabaseInsertHelper.insertClient(client);
-            System.out.println("New client ID at: " + clientId);
-            
-            // get client and its address from database
-            client = DatabaseSelectHelper.getClient(clientId);
-            System.out.println("Retrieved results from database:");
-            System.out.println(client);
-            System.out.println(client.getAddress());
-            
-            // 
-            IAssessmentBuilder assessBuilder = new AssessmentBuilder();
-            assessBuilder = (IAssessmentBuilder) assessBuilder
-                    .setId(1)
-                    .setClientId(clientId)
-                    .setLanguage("English")
-                    .setPostalCode("M4H2A5")
-                    .setReferredBy("Method of referral")
-                    .setOrganizationType("Organization type")
-                    .setUpdateReason("Reason for update")
-                    .setServiceType("A")
-                    .setEssentialSkills(Arrays.asList("Computer skills", "Numeracy"))
-                    .setSupportServices(Arrays.asList("Transportation, Translation"))
-                    .setTargetGroups(Arrays.asList("Senior", "Refugees"));
-            Assessment assessment = assessBuilder
-                    .setStartDate("2017-01-01")
-                    .setLanguageGoal("Language skill goal")
-                    .setOtherGoal("Other skill goal")
-                    .setIntendsCitizenship(true)
-                    .setReqSupportServices(true)
-                    .setPlanComplete(true)
-                    .setEndDate("2018-01-01")
-                    .setFindEmployment("Time frame", "Min experience", "Skill level", "Wants to intent")
-                    .create();
-            assessment.addIncrease("Social networks", true);
-            assessment.addNonIRCCService("Financial");
-            
-            int serviceId = DatabaseInsertHelper.insertAssessment(assessment);
-            System.out.println("New service ID at: " + serviceId);
-            
-            assessment = DatabaseSelectHelper.getAssessment(serviceId);
-            System.out.println(assessment);
-            System.out.println(assessment.getFindEmployment());
-        }
         
     }
     
