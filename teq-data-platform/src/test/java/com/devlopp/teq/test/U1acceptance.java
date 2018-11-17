b7fb775eeb5c6a3341a1093f3359c60c1aa1f085
Jeffrey L <jeffclub51@yahoo.ca>
2018-11-16 23:11:51 -0500

    Fixed formatting for unit tests

--- teq-data-platform/src/test/java/com/devlopp/teq/test/U1acceptance.java
+++ teq-data-platform/src/test/java/com/devlopp/teq/test/U1acceptance.java
@@ -10,36 +10,28 @@ import com.devlopp.teq.databasehelper.DatabaseInsertHelper;
 import com.devlopp.teq.databasehelper.DatabaseSelectHelper;
 
 public class U1acceptance {
-	
-	public void u1_acceptance_test() {
-	   
-	   String filePath = "client_profile.xlsx";
-       System.out.println("TEQ LIP data platform");
-       
-       boolean dbExists = DatabaseDriverHelper.databaseExists();
-       System.out.println("Database exists: " + dbExists);
-       
-	        if (!dbExists) {
-	            DatabaseDriverHelper.initializeDatabase();
-	        }
-
-	        List<Client> clients = ExcelDriver.readClientProfile(filePath,0);
-	        for (Client client : clients) {
-	            
-	            // insert address first
-	            Address address = client.getAddress();
-	            int addressId = DatabaseInsertHelper.insertAddress(address);
-	            System.out.println("New address ID at: " + addressId);
-	            
-	            // insert client next
-	            int clientId = DatabaseInsertHelper.insertClient(client);
-	            System.out.println("New client ID at: " + clientId);
-	            
-	            // get client and its address from database
-	            client = DatabaseSelectHelper.getClient(clientId);
-	            System.out.println("Retrieved results from database:");
-	            System.out.println(client);
-	            System.out.println(client.getAddress());
-	        }
-	}
+    public void u1_acceptance_test() {
+        String filePath = "client_profile.xlsx";
+        System.out.println("TEQ LIP data platform");
+        boolean dbExists = DatabaseDriverHelper.databaseExists();
+        System.out.println("Database exists: " + dbExists);
+        if (!dbExists) {
+            DatabaseDriverHelper.initializeDatabase();
+        }
+        List<Client> clients = ExcelDriver.readClientProfile(filePath, 0);
+        for (Client client : clients) {
+            // insert address first
+            Address address = client.getAddress();
+            int addressId = DatabaseInsertHelper.insertAddress(address);
+            System.out.println("New address ID at: " + addressId);
+            // insert client next
+            int clientId = DatabaseInsertHelper.insertClient(client);
+            System.out.println("New client ID at: " + clientId);
+            // get client and its address from database
+            client = DatabaseSelectHelper.getClient(clientId);
+            System.out.println("Retrieved results from database:");
+            System.out.println(client);
+            System.out.println(client.getAddress());
+        }
+    }
 }
